d4b0d91eca06b0eeb4d578d9afb91757d35ec3c7
Bodaniel Jeanes <me@bjeanes.com>
2008-12-03 00:26:41 +1000

    changed the state and state transitions to be simpler and dryer

--- app/models/phone/states.rb
+++ app/models/phone/states.rb
@@ -1,24 +1,19 @@
 module Phone::States
   include AlterEgo
   
-  state :on_hold, :default => true do
-    handle :state { "on_hold" }
+  state :paused, :default => true do
+    handle(:state) { "paused" }
     transition :to => :charging, :on => :charge!
-    transition :to => :paused,   :on => :pause!
   end
   
   state :charging do
-    handle :state { "charging" }
+    handle(:state) { "charging" }
     transition :to => :completed_charging, :on => :complete!
-  end
-  
-  state :paused do
-    handle :state { "paused" }
-    transition :to => :charging, :on => :charge!
+    transition :to => :paused,   :on => :pause!
   end
   
   state :completed_charging do
-    handle :state { "completed_charging" }
+    handle(:state) { "completed_charging" }
     transition :to => :picked_up, :on => :pick_up!
   end
 end
