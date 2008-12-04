e1821c6f7f3bf4c4b2fa6a138bd9254b3d8e2a4b
Bodaniel Jeanes <me@bjeanes.com>
2008-12-05 00:23:14 +1000

    Removed unneeded migration

--- app/models/phone/states.rb
+++ app/models/phone/states.rb
@@ -1,19 +1,23 @@
 module Phone::States
-  include AlterEgo
-  
-  state :paused, :default => true do
-    handle(:state) { "paused" }
-    transition :to => :charging, :on => :charge!
-  end
-  
-  state :charging do
-    handle(:state) { "charging" }
-    transition :to => :completed_charging, :on => :complete!
-    transition :to => :paused,   :on => :pause!
-  end
-  
-  state :completed_charging do
-    handle(:state) { "completed_charging" }
-    transition :to => :picked_up, :on => :pick_up!
+  def self.included(klass)
+    klass.instance_eval do
+      include AlterEgo
+
+      state :paused, :default => true do
+        handle(:state) { "paused" }
+        transition :to => :charging, :on => :charge!
+      end
+
+      state :charging do
+        handle(:state) { "charging" }
+        transition :to => :completed_charging, :on => :complete!
+        transition :to => :paused,   :on => :pause!
+      end
+
+      state :completed_charging do
+        handle(:state) { "completed_charging" }
+        transition :to => :picked_up, :on => :pick_up!
+      end
+    end
   end
 end
--- db/migrate/20081202142450_rename_state_column_to_status_on_phones.rb
+++
@@ -1,9 +0,0 @@
-class RenameStateColumnToStatusOnPhones < ActiveRecord::Migration
-  def self.up
-    rename_column :phones, :state, :status
-  end
-
-  def self.down
-    rename_column :phones, :status, :state
-  end
-end
